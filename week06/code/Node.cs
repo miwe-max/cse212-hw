5ff5fbe99383ca3cd77a837ca10bae584cc576e3
miwe-max <miwe@byupathway.edu>
2026-04-12 21:59:00 +0100

    Completed W06 Code: Trees, with 9 test and extra credit

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,9 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        // If value already exists, do nothing
+    if (value == Data)
+        return;
 
         if (value < Data)
         {
@@ -34,12 +37,20 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        return true;
+
+    if (value < Data)
+        return Left != null && Left.Contains(value);
+    else
+        return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+       int leftHeight = Left != null ? Left.GetHeight() : 0;
+    int rightHeight = Right != null ? Right.GetHeight() : 0;
+    return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
